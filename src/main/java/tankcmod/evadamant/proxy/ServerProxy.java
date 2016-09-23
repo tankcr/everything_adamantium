ad24359d65f0792165954a2a7c8bb38d00b55d2e
tankcr <kristopher.roy@gmail.com>
2016-09-22 23:03:27 -0700

    updated oregen

--- src/main/java/tankcmod/evadamant/proxy/ServerProxy.java
+++ src/main/java/tankcmod/evadamant/proxy/ServerProxy.java
@@ -3,6 +3,8 @@ package tankcmod.evadamant.proxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import tankcmod.evadamant.world.OreGen;
 
 /**
  * Created by KRoy.Local on 9/21/2016.
@@ -12,6 +14,8 @@ public class ServerProxy extends CommonProxy {
     @Override
     public void preInit(FMLPreInitializationEvent e) {
         super.preInit(e);
+        OreGen ore_gen = new OreGen();
+        GameRegistry.registerWorldGenerator(ore_gen, 0);
     }
 
     @Override
--- src/main/java/tankcmod/evadamant/world/OreGen.java
+++ src/main/java/tankcmod/evadamant/world/OreGen.java
@@ -1,60 +1,45 @@
 package tankcmod.evadamant.world;
 
+import com.google.common.base.Predicate;
 import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkGenerator;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 import net.minecraftforge.fml.common.IWorldGenerator;
-import tankcmod.evadamant.blocks.ADMTOre;
 import tankcmod.evadamant.ModBlocks;
 
 import java.util.Random;
 
-public class OreGen implements IWorldGenerator
-{
+public class OreGen implements IWorldGenerator {
+
     @Override
-    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider)
-    {
-        switch(world.provider.getDimension())
-        {
-            case -1:
-                generateNether(world, random, chunkX * 16, chunkZ * 16);
-                break;
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator,
+                         IChunkProvider chunkProvider) {
+        switch (world.provider.getDimension()) {
             case 0:
-                generateSurface(world, random, chunkX * 16, chunkZ * 16);
+                generateOverworld(world, random, chunkX, chunkZ);
                 break;
-            case 1:
-                generateEnd(world, random, chunkX * 16, chunkZ * 16);
-                break;
-            default:
         }
+
     }
 
-    private void generateEnd(World world, Random random, int x, int z)
-    {
+    public void generateOverworld(World world, Random rand, int x, int z) {
+        generateOre(ModBlocks.admtOre, world, rand, x, z, 2, 6, 4, 5, 68, Blocks.STONE);
     }
 
-    private void generateSurface(World world, Random random, int x, int z)
-    {
-        generateOre(ModBlocks.admtOre, world, random, x, z)
-    }
-
-    private void generateNether(World world, Random random, int x, int z)
-    {
-    }
-
-    public void generateOre(Block block, World world, Random random, int chunk_x, int chunk_z, int maxX, int maxZ, int maxVeinSize, int chancesToSpawn, int minY, int maxY, Block generateIn)
-    {
-
-
+    public void generateOre(Block block, World world, Random random, int chunkX, int chunkZ, int minVienSize, int maxVienSize, int chance, int minY, int maxY, Block generateIn) {
+        int vienSize = minVienSize + random.nextInt(maxVienSize - minVienSize);
         int heightRange = maxY - minY;
-        WorldGenMinable worldgenminable = new WorldGenMinable(block, maxVeinSize, generateIn);
-        for (int k1 = 0; k1 < chancesToSpawn; ++k1)
-        {
-            int xrand = random.nextInt(16);
-            int yrand = random.nextInt(heightRange) + minY;
-            int zrand = random.nextInt(16);
-            worldgenminable.generate(world, random, chunk_x+xrand, y, chunk_z+zrand);
+        WorldGenMinable gen = new WorldGenMinable((IBlockState) block, vienSize, (Predicate<IBlockState>) generateIn);
+        for(int i = 0; i < chance; i++) {
+            int xRand = chunkX * 16 + random.nextInt(16);
+            int yRand = random.nextInt(heightRange) + minY;
+            int zRand = chunkZ * 16 + random.nextInt(16);
+            gen.generate(world, random, new BlockPos(xRand, yRand, zRand));
         }
     }
 }
